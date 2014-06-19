0ad3442675e32815ac3851b04ddc42498b9cdc6c
wat-wat <something@fake-url.com>
2014-06-19 03:49:37 -0700

    Stupid Typo bug.

--- ScrapePlugins/CzLoader/czContentLoader.py
+++ ScrapePlugins/CzLoader/czContentLoader.py
@@ -141,7 +141,7 @@ class CzContentLoader(ScrapePlugins.RetreivalDbBase.ScraperDbBase):
 
 		fileUrl = self.getDownloadUrl(sourceUrl, sourceFn)
 		if fileUrl is None:
-			self.log.error()("Could not find url!")
+			self.log.error("Could not find url!")
 			self.updateDbEntry(link["sourceUrl"], dlState=-1)
 			# self.deleteRowsByValue(sourceUrl=sourceUrl)
 			return
