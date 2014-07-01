ddfd10252378f9f1141d99f4efc1b9d59a8efbe9
wat-wat <something@fake-url.com>
2014-07-01 00:04:30 -0700

    Updated settings file with deduplicator stuff.

--- settings.base.py
+++ settings.base.py
@@ -25,7 +25,10 @@ badImageDir  = r"/SOMETHING/MangaCMS/removeImages"
 # This is the path to the deduplication tool database API python file.
 # You only need to set this if you want to use the on-the-fly duplicate
 # removal, which is complex and not fully finished at this time.
-dedupApiFile = None
+# You must have https://github.com/fake-name/IntraArchiveDeduplicator somewhere,
+# and have allowed it to build a database of the extant local files for it to
+# be of any use.
+dedupApiFile = '/SOMETHING/Deduper/dbApi.py'
 
 # Folders to scan for folders to use as download paths.
 # Directories are scanned by sorted keys
