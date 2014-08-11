31681834c1bbe25c4ddb7bec10750827b9774644
wat-wat <something@fake-url.com>
2014-08-11 02:00:25 -0700

    Info loading stuff for ViScans IRC things. Needs the IRC grabber to work, but the thing the IRC grabber should grab are there.

---
+++ tests/test-viLoad.py
@@ -0,0 +1,36 @@
+
+import logSetup
+if __name__ == "__main__":
+	logSetup.initLogging()
+
+
+
+import runStatus
+
+from ScrapePlugins.IrcGrabber.ViScans.ViScrape import ViTriggerLoader
+import signal
+
+import os.path
+
+def customHandler(signum, stackframe):
+	if runStatus.run:
+		runStatus.run = False
+		print("Telling threads to stop")
+	else:
+		print("Multiple keyboard interrupts. Raising")
+		raise KeyboardInterrupt
+
+def test():
+
+
+	cl = ViTriggerLoader()
+	cl.go()
+
+
+if __name__ == "__main__":
+	try:
+		test()
+	finally:
+		import nameTools as nt
+		nt.dirNameProxy.stop()
+
