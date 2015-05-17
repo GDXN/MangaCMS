a9797e122e1e8e70c1ac7214a446a86cf04f24eb
Fake-Name <something@fake-url.com>
2015-05-16 20:37:03 -0700

    FeedParser thingies.

--- FeedScrape/FeedDataParser.py
+++ FeedScrape/FeedDataParser.py
@@ -1,16 +1,32 @@
 
 #!/usr/bin/python
 # from profilehooks import profile
-
+import urllib.parse
 import abc
 import json
 # pylint: disable=W0201
+
+
+skip_filter = [
+	"www.baka-tsuki.org",
+	"re-monster.wikia.com",
+]
+
+def getNiceName(srcUrl):
+	return False
+
 
 class DataParser():
 
 	amqpint = None
 
 	def getRawFeedMessage(self, feedDat):
+
+		netloc = getNiceName(feedDat['linkUrl'])
+		if not netloc:
+			netloc = urllib.parse.urlparse(feedDat['linkUrl']).netloc
+		feedDat['srcname'] = netloc
+
 		ret = {
 			'type' : 'raw-feed',
 			'data' : feedDat
@@ -19,4 +35,8 @@ class DataParser():
 
 	def processFeedData(self, feedDat):
 
-		self.amqpint.put_item(self.getRawFeedMessage(feedDat))
+		if any([item in feedDat['linkUrl'] for item in skip_filter]):
+			return
+
+		raw = self.getRawFeedMessage(feedDat)
+		self.amqpint.put_item(raw)
