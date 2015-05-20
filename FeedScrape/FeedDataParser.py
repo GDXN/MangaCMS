0fba020891dc851c1a00d5a67520db215a8be395
Fake-Name <something@fake-url.com>
2015-05-19 20:41:45 -0700

    New feed parsers.

--- FeedScrape/FeedDataParser.py
+++ FeedScrape/FeedDataParser.py
@@ -16,6 +16,37 @@ skip_filter = [
 	"re-monster.wikia.com",
 ]
 
+
+
+
+def extractChapterVol(inStr):
+
+	# Becuase some series have numbers in their title, we need to preferrentially
+	# chose numbers preceeded by known "chapter" strings when we're looking for chapter numbers
+	# and only fall back to any numbers (chpRe2) if the search-by-prefix has failed.
+	chpRe1 = re.compile(r"(?<!volume)(?<!vol)(?<!v)(?<!of)(?<!season) ?(?:chapter |ch|c)(?: |_|\.)?((?:\d+)|(?:\d+\.)|(?:\.\d+)|(?:\d+\.\d+))", re.IGNORECASE)
+	chpRe2 = re.compile(r"(?<!volume)(?<!vol)(?<!v)(?<!of)(?<!season) ?(?: |_)(?: |_|\.)?((?:\d+)|(?:\d+\.)|(?:\.\d+)|(?:\d+\.\d+))", re.IGNORECASE)
+	volRe  = re.compile(r"(?: |_|\-)(?:volume|vol|v|season)(?: |_|\.)?((?:\d+)|(?:\d+\.)|(?:\.\d+)|(?:\d+\.\d+))", re.IGNORECASE)
+
+	chap = None
+	for chRe in [chpRe1, chpRe2]:
+		chapF = chRe.findall(inStr)
+		if chapF:
+			chap  = float(chapF.pop(0)) if chapF else None
+		if chap != None:
+			break
+
+	volKey = volRe.findall(inStr)
+	vol    = float(volKey.pop(0))  if volKey    else None
+
+	chap   = chap if chap != None else 0.0
+	vol    = vol  if vol  != None else 0.0
+
+	if vol == 0:
+		vol = None
+	return chap, vol
+
+
 def buildReleaseMessage(raw_item, series, vol, chap, postfix=''):
 	'''
 	Special case behaviour:
@@ -86,8 +117,6 @@ class DataParser():
 	####################################################################################################################################################
 
 	def extractOniichanyamete(self, item):
-		# print('"{}" "{}"'.format(item['title'], item['tags']))
-
 		tilea_norm       = re.search(r'^(Tilea’s Worries) – Chapter (\d+)$',               item['title'])
 		tilea_extra      = re.search(r'^(Tilea’s Worries) – ([^0-9]*?)$',                  item['title'])
 		other_world_norm = re.search(r'^(I’m Back in the Other World\?) – Chapter (\d+)$', item['title'])
@@ -164,7 +193,144 @@ class DataParser():
 
 			return buildReleaseMessage(item, series, vol, chp)
 
-		# print(item['title'])
+		return False
+
+	####################################################################################################################################################
+	# Yoraikun
+	####################################################################################################################################################
+
+
+	def extractYoraikun(self, item):
+
+		if 'The Rise of the Shield Hero' in item['tags']:
+			chp, vol = extractChapterVol(item['title'])
+			if vol == 0:
+				vol = None
+
+			return buildReleaseMessage(item, 'The Rise of the Shield Hero', vol, chp)
+
+		elif 'Konjiki no Wordmaster' in item['tags']:
+			chp, vol = extractChapterVol(item['title'])
+			if vol == 0:
+				vol = None
+
+			return buildReleaseMessage(item, 'Konjiki no Wordmaster', vol, chp)
+
+		return False
+
+	####################################################################################################################################################
+	# FlowerBridgeToo
+	####################################################################################################################################################
+
+
+	def extractFlowerBridgeToo(self, item):
+		# Seriously, you were too lazy to type out the *tags*?
+		# You only have to do it ONCE!
+		if 'MGA Translation' in item['tags']:
+			chp, vol = extractChapterVol(item['title'])
+			# Also called "Martial God Asura"
+			return buildReleaseMessage(item, 'Xiuluo Wushen', vol, chp)
+		elif 'Xian Ni' in item['tags']:
+			chp, vol = extractChapterVol(item['title'])
+			return buildReleaseMessage(item, 'Xian Ni', vol, chp)
+
+		return False
+
+
+	####################################################################################################################################################
+	# Gravity Translation
+	####################################################################################################################################################
+
+
+	def extractGravityTranslation(self, item):
+
+		chp, vol = extractChapterVol(item['title'])
+		if 'Zhan Long' in item['tags']:
+			return buildReleaseMessage(item, 'Zhan Long', vol, chp)
+		elif 'Battle Through the Heavens' in item['tags']:
+			return buildReleaseMessage(item, 'Battle Through the Heavens', vol, chp)
+		elif "Ascension of the Alchemist God" in item['title'] or "TAG Chapter" in item['title']:
+			return buildReleaseMessage(item, 'Ascension of the Alchemist God', vol, chp)
+
+		return False
+
+
+	####################################################################################################################################################
+	# Pika Translations
+	####################################################################################################################################################
+
+
+	def extractPikaTranslations(self, item):
+		chp, vol = extractChapterVol(item['title'])
+		if 'Close Combat Mage' in item['tags']:
+			return buildReleaseMessage(item, 'Close Combat Mage', vol, chp)
+
+		return False
+
+
+
+	####################################################################################################################################################
+	# Blue Silver Translations
+	####################################################################################################################################################
+
+
+	def extractBlueSilverTranslations(self, item):
+
+		if 'Douluo Dalu' in item['tags']:
+
+			# All the releases start with the chapter number.
+			# This check is only needed because there are a few releases of
+			# related things that are annoyingly tagged in as well.
+			if not all([val in '0123456789' for val in item['title'].split()[0]]):
+				return False
+
+			proc_str = "%s %s" % (item['tags'], item['title'])
+			proc_str = proc_str.replace("'", " ")
+			chp, vol = extractChapterVol(proc_str)
+
+			return buildReleaseMessage(item, 'Douluo Dalu', vol, chp)
+		return False
+
+
+
+	####################################################################################################################################################
+	# Alyschu & Co
+	####################################################################################################################################################
+
+
+	def extractAlyschuCo(self, item):
+		# Whyyyy would you do these bullshit preview things!
+		if "PREVIEW" in item['title'] or "preview" in item['title']:
+			return False
+
+		chp, vol = extractChapterVol(item['title'])
+
+		if 'Against the Gods' in item['tags'] or 'Ni Tian Xie Shen (Against the Gods)' in item['title']:
+			return buildReleaseMessage(item, 'Against the Gods', vol, chp)
+		elif 'The Simple Life of Killing Demons' in item['tags']:
+			return buildReleaseMessage(item, 'The Simple Life of Killing Demons', vol, chp)
+		elif 'Magic, Mechanics, Shuraba' in item['title']:
+			return buildReleaseMessage(item, 'Magic, Mechanics, Shuraba', vol, chp)
+		elif 'The Flower Offering' in item['tags']:
+			return buildReleaseMessage(item, 'The Flower Offering', vol, chp)
+
+		return False
+
+
+
+	####################################################################################################################################################
+	# Henouji Translation
+	####################################################################################################################################################
+
+
+	def extractHenoujiTranslation(self, item):
+		# fffuuuu "last part" is not a helpful title!
+		chp, vol = extractChapterVol(item['title'])
+		# print(item['title'], item['tags'])
+
+		# if 'Against the Gods' in item['tags'] or 'Ni Tian Xie Shen (Against the Gods)' in item['title']:
+		# 	return buildReleaseMessage(item, 'Against the Gods', vol, chp)
+
 		return False
 
 
@@ -180,15 +346,43 @@ class DataParser():
 	def dispatchRelease(self, item):
 
 		if item['srcname'] == 'Sousetsuka':
+			# return None
 			return self.extractSousetsuka(item)
 		if item['srcname'] == 'お兄ちゃん、やめてぇ！':
+			# return None
 			return self.extractOniichanyamete(item)
 		if item['srcname'] == 'Natsu TL':
+			# return None
 			return self.extractNatsuTl(item)
 		if item['srcname'] == 'TheLazy9':
+			# return None
 			return self.extractTheLazy9(item)
+		if item['srcname'] == 'Yoraikun Translation':
+			# return None
+			return self.extractYoraikun(item)
+		if item['srcname'] == 'Flower Bridge Too':
+			# return None
+			return self.extractFlowerBridgeToo(item)
+		if item['srcname'] == 'Gravity Translation':
+			# return None
+			return self.extractGravityTranslation(item)
+		if item['srcname'] == 'Pika Translations':
+			# return None
+			return self.extractPikaTranslations(item)
+		if item['srcname'] == 'Blue Silver Translations':
+			# return None
+			return self.extractBlueSilverTranslations(item)
+		if item['srcname'] == 'Alyschu & Co':
+			# return None
+			return self.extractAlyschuCo(item)
+		if item['srcname'] == 'Henouji Translation':
+			return self.extractHenoujiTranslation(item)
+			# return None
+
+		# if item['srcname'] == 'Flower Bridge Too':
+		# 	return self.extractYoraikun(item)
 		# else:
-		# 	print(item)
+		# print("'%s' '%s' '%s'" % (item['srcname'], item['title'], item['tags']))
 
 		return False
 
